25279f0019a7e5373c8069fa1eaaaf7cd1327036
Gaurav Dubey <gauravd638@gmail.com>
2020-12-11 23:51:45 +0530

    delete functionlity added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import firebase from 'firebase';
 
 function App() {
   const [todos, setTodos] = useState([
-    'abc','def'
+    
   ]);
   const [input, setInput] = useState('');
 
@@ -17,13 +17,13 @@ function App() {
     //this code here fires when app loads
     db.collection('todos').orderBy('timestamp','desc').onSnapshot(snapshot =>{
       
-      setTodos(snapshot.docs.map(doc => doc.data().todo))
+      setTodos(snapshot.docs.map(doc => ({id:doc.id,todo:doc.data().todo})))
     }
   )},[]);
   const addTodo = (event) => {
     //this will fire off when we click the button
     event.preventDefault(); //it will stop refreshing the page 
-    db.collection('todo').add({
+    db.collection('todos').add({
       todo:input,
       timestamp:firebase.firestore.FieldValue.serverTimestamp()
     })
@@ -46,16 +46,16 @@ function App() {
         </FormControl>
 
         <Button disabled ={!input}  type="submit" onClick={addTodo} variant="contained" color="primary">
-        Add todo
+        Add Todo
 </Button>
 
-      {/*   <button  >Add todo</button> */}
+      
       </form>
 
       <ul>
         {todos.map(todo => (
-          <Todo text ={todo}/>
-          // <li>{todo}</li>
+          <Todo todo ={todo}/>
+          
         ))}
       </ul>
     </div>
--- src/Todo.js
+++ src/Todo.js
@@ -1,19 +1,27 @@
-import {  List,ListItem, ListItemText } from "@material-ui/core";
-import './Todo.css'
 import React from "react";
-/* import db from './firebase'; */
 
 
- function Todo(props) {
+import {
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Button
+} from "@material-ui/core";
+import "./Todo.css";
+import db from './firebase';
+
+function Todo(props) {
   //props = properties
   return (
     <List className="todo__list">
-        <ListItem>
-          
-            <ListItemText primary={props.text} secondary="Dummy Lines" />
-        </ListItem>
-{/*         <Button onClick = {event => db.collection('todo').doc(props.todo.id).delete()}
-    >Delete Me</Button> */}
+      <ListItem>
+        <ListItemAvatar></ListItemAvatar>
+
+        <ListItemText primary={props.todo.todo} secondary="Dummy Lines" />
+      </ListItem>
+              <Button onClick = {event => db.collection('todos').doc(props.todo.id).delete()}
+    >Delete Me</Button>
     </List>
   )
 }
