fda2cb23dcd9db332a75ac6d4393774839108642
Gaurav Dubey <gauravd638@gmail.com>
2020-12-12 23:52:41 +0530

    modal added for edit

--- src/Todo.js
+++ src/Todo.js
@@ -10,19 +10,35 @@ import {
 } from "@material-ui/core";
 import "./Todo.css";
 import db from './firebase';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 function Todo(props) {
   //props = properties
+  const [open,setOpen] = useState(fasle);
+  const handleOpen = () =>{
+      setOpen(true);  
+  };
+  
   return (
+      <>
+        <Modal
+  open={open}
+  onClose={e =>setOpen(false)}
+>
+  
+</Modal>
+
     <List className="todo__list">
       <ListItem>
         <ListItemAvatar></ListItemAvatar>
 
         <ListItemText primary={props.todo.todo} secondary="Dummy Lines" />
       </ListItem>
-              <Button onClick = {event => db.collection('todos').doc(props.todo.id).delete()}
-    >Delete Me</Button>
+              <DeleteForeverIcon onClick = {event => db.collection('todos').doc(props.todo.id).delete()}/>  
+              
+    
     </List>
+    </>
   )
 }
 
